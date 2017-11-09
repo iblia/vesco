e0fd49fd1f9796170a85102f7f44b1d7b10caad6
Marta <martabobowskamichalik@gmail.com>
2017-11-10 00:46:30 +0100

    pierwszy

---
+++ js/custom.js
@@ -0,0 +1,64 @@
+/* ============================================
+                    SERVICES
+=============================================*/
+$(function () {
+    //animate on scroll
+    new WOW().init();
+});
+/* ============================================
+                    WORK
+=============================================*/
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
+/* ============================================
+                    TEAM
+=============================================*/
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
+/* ============================================
+                    TESTIMONIALS
+=============================================*/
+$(function () {
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
+/* ============================================
+                   STATS
+=============================================*/
+$(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+/* ============================================
+                    CLIENTS
+=============================================*/
+$(function () {
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
